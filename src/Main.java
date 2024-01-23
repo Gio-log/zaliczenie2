7647f74bdef563957a3d665c7a4ea3a604044f47
Gio-log <gierimaniak@gmail.com>
2024-01-23 12:03:09 +0100

    finished task

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,90 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        IConvert[] Points = new IConvert[6];
-        Points[0] = new CMove();
-        Points[1] = new CMove(2,2,2,2);
-        Points[2] = new CMove(9,29,13,3);
-        Points[3] = new CRotate();
-        Points[4] = new CRotate(2,2,2);
-        Points[5] = new CRotate(23,11,32);
-        System.out.println(Points[0].toString());
-        System.out.println(Points[1].toString());
-        System.out.println(Points[2].toString());
-        System.out.println(Points[3].toString());
-        System.out.println(Points[4].toString());
-        System.out.println(Points[5].toString());
-        
+        Scanner scanner = new Scanner(System.in);
+        ArrayList<IConvert> Points= new ArrayList<>();
+        Points.add(new CMove());
+        Points.add(new CMove(2,2,2,2));
+        Points.add(new CMove(9,29,13,3));
+        Points.add(new CRotate());
+        Points.add(new CRotate(2,2,2));
+        Points.add(new CRotate(23,11,32));
+        /*
+        Point staticMove = CMove.doConvert(new Point(2,2),2,2);
+        Point staticRotate = CRotate.doConvert(new Point(2,2),2);
+        System.out.println(Points.get(0).toString());
+        System.out.println(Points.get(1).toString());
+        System.out.println(Points.get(2).toString());
+        System.out.println(Points.get(3).toString());
+        System.out.println(Points.get(4).toString());
+        System.out.println(Points.get(5).toString());
+        System.out.println(staticMove);
+        System.out.println(staticRotate);
+         */
+        while(true){
+            System.out.println("\n\nProszę wybrać numer: Move[1], Rotate[2], Wyświetl kolekcje");
+            String profil = scanner.nextLine().trim();
+            if(profil.equals("1"))
+            {
+                try {
+                    System.out.println("\nProszę podać parametry punktu i przekształcenia\nX = ");
+                    int X = Integer.parseInt(scanner.nextLine().trim());
+                    if(X>1000*CConvert.NR||X<-1000*CConvert.NR)
+                    {
+                        throw new IllegalArgumentException();
+                    }
+                    System.out.println("\nY = ");
+                    int Y = Integer.parseInt(scanner.nextLine().trim());
+                    if(Y>1000*CConvert.NR||Y<-1000*CConvert.NR)
+                    {
+                        throw new IllegalArgumentException();
+                    }
+                    System.out.println("\nPrzemieszczenie X = ");
+                    int PX = Integer.parseInt(scanner.nextLine().trim());
+                    System.out.println("\nPrzemieszczenie Y = ");
+                    int PY = Integer.parseInt(scanner.nextLine().trim());
+                    Points.add(new CMove(X,Y,PX,PY));
+                }catch(Exception e){
+                    System.out.println("\nPodano niepoprawny parametr");
+                }
+            }
+            else if (profil.equals("2"))
+            {
+                try {
+                    System.out.println("\nProszę podać parametry punktu i przekształcenia\nX = ");
+                    int X = Integer.parseInt(scanner.nextLine().trim());
+                    if(X>1000*CConvert.NR||X<-1000*CConvert.NR)
+                    {
+                        throw new IllegalArgumentException();
+                    }
+                    System.out.println("\nY = ");
+                    int Y = Integer.parseInt(scanner.nextLine().trim());
+                    if(Y>1000*CConvert.NR||Y<-1000*CConvert.NR)
+                    {
+                        throw new IllegalArgumentException();
+                    }
+                    System.out.println("\nEta = ");
+                    int Eta = Integer.parseInt(scanner.nextLine().trim());
+                    Points.add(new CRotate(X,Y,Eta));
+                }catch(Exception e){
+                    System.out.println("\nPodano niepoprawny parametr");
+                }
+            }
+            else if (profil.equals("3"))
+            {
+                for (IConvert iteration: Points) {
+                    System.out.println(iteration.toString());
+                }
+            }
+            System.out.println("\nCzy chcesz kontynuować?(y/n)\n");
+            String repeat =scanner.nextLine().trim();
+            if(repeat.equals("n")||repeat.equals("N"))
+            {
+                break;
+            }
+        }
     }
 }
